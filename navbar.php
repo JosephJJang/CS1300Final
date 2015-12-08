e6d4c417d339f6eb1e1bd748f8475da84e6bd3d6
Joseph Jang <jj465@cornell.edu>
2015-12-08 15:06:37 -0500

    navbar.php

---
+++ navbar.php
@@ -0,0 +1,12 @@
+<div id="navbar">
+    <div class="sticky">
+        <div class="container">
+            <a href="./index.php"><div class="nav-btn"><div class="nav-btn-text">HOME</div></div></a>
+            <a href="./about.php"><div class="nav-btn"><div class="nav-btn-text">ABOUT</div></div></a>
+            <a href="./gallery.php"><div class="nav-btn"><div class="nav-btn-text">GALLERY</div></div></a>
+            <
+            <a href="./contact.php"><div class="nav-btn"><div class="nav-btn-text">CONTACT</div></div></a>
+            <a href="./index.php"><div id="navTitle"><div class="nav-btn-text">Hong's Gallery</div></div></a>
+        </div>
+    </div>
+</div>
