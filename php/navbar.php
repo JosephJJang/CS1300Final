0d275b82da1e32a787ddb25bf36fae5f44a380ec
Joseph Jang <jj465@cornell.edu>
2015-12-08 15:40:38 -0500

    navbar 4

--- php/navbar.php
+++ php/navbar.php
@@ -1,15 +1,15 @@
 <div id="navbar">
     <div class="sticky">
-        <div class="container">
+        <ul class="container">
             <a href="./index.php"><div class="nav-btn"><div class="nav-btn-text">HOME</div></div></a>
             <a href="./about.php"><div class="nav-btn"><div class="nav-btn-text">ABOUT</div></div></a>
             <a href="./gallery.php"><div class="nav-btn"><div class="nav-btn-text">GALLERY</div></div></a>
-            <ul class = "sub-menu">
-              <li><a href = "./gallery.php"><div class = "nav-btn"><div class = "nav-btn-text">GALLERY PAGE 1</div></div></a></li>
-              <li><a href = "./gallery.php"><div class = "nav-btn"><div class = "nav-btn-text">GALLERY PAGE 2</div></div></a></li>
-            </ul>
+              <div class = "sub-menu">
+                <a href = "./gallery.php"><div class = "nav-btn"><div class = "nav-btn-text">GALLERY PAGE 1</div></div></a>
+                <a href = "./gallery.php"><div class = "nav-btn"><div class = "nav-btn-text">GALLERY PAGE 2</div></div></a>
+              </div>
             <a href="./contact.php"><div class="nav-btn"><div class="nav-btn-text">CONTACT</div></div></a>
             <a href="./index.php"><div id="navTitle"><div class="nav-btn-text">Hong's Gallery</div></div></a>
-        </div>
+        </ul>
     </div>
 </div>
