a8be32bd1106a05c2007f77b10d5b2c23758fe22
Joseph Jang <jj465@cornell.edu>
2015-12-08 15:09:32 -0500

    navbar ch1

--- navbar.php
+++ navbar.php
@@ -4,7 +4,10 @@
             <a href="./index.php"><div class="nav-btn"><div class="nav-btn-text">HOME</div></div></a>
             <a href="./about.php"><div class="nav-btn"><div class="nav-btn-text">ABOUT</div></div></a>
             <a href="./gallery.php"><div class="nav-btn"><div class="nav-btn-text">GALLERY</div></div></a>
-            <
+            <ul class = "sub-menu">
+              <li><a href = "./gallery.php"><div class = "nav-btn"><div class = "nav-btn-text">GALLERY PAGE 1</div></div></a>
+              <li><a href = "./gallery.php"><div class = "nav-btn"><div class = "nav-btn-text">GALLERY PAGE 2</div></div></a>
+            </ul>
             <a href="./contact.php"><div class="nav-btn"><div class="nav-btn-text">CONTACT</div></div></a>
             <a href="./index.php"><div id="navTitle"><div class="nav-btn-text">Hong's Gallery</div></div></a>
         </div>
